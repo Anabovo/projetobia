699614ea6a247bd9ea3715387037b9749fe27009
Anabovo <sousabovoana@gmail.com>
2024-08-19 11:46:18 -0300

    dia 1/08/2024

--- projetobia/CadastroPedido.xaml.cs
+++ projetobia/CadastroPedido.xaml.cs
@@ -7,23 +7,12 @@ namespace projetobia
         public CadastroPedido()
         {
             InitializeComponent();
-            ConfirmarButton.Clicked += OnConfirmarButtonClicked;
         }
 
-        private void OnConfirmarButtonClicked(object sender, EventArgs e)
+        private void VoltarDoPedido(object sender, EventArgs e)
         {
-            // Implementar lógica do botão Confirmar aqui
-            string codigoCliente = CodigoClienteEntry.Text;
-            string telefone = TelefoneEntry.Text;
-            string endereco = EnderecoEntry.Text;
-            string codigoProduto = CodigoProdutoEntry.Text;
-            string cor = CorEntry.Text;
-            string quantidade = QuantidadeEntry.Text;
-            string frete = FreteEntry.Text;
-            string valorTotal = ValorTotalEntry.Text;
-
-            // Exemplo: Exibir uma mensagem de confirmação
-            DisplayAlert("Pedido Confirmado", "Seu pedido foi confirmado!", "OK");
+            if (Application.Current != null)
+                Application.Current.MainPage = new TelaInicial();
         }
     }
 }
--- projetobia/CadastroTransportadora.xaml.cs
+++ projetobia/CadastroTransportadora.xaml.cs
@@ -9,19 +9,5 @@ namespace projetobia
         {
             InitializeComponent();
         }
-
-        private void OnFeitoClicked(object sender, EventArgs e)
-        {
-            // Aqui você pode adicionar a lógica que deseja executar quando o botão for clicado.
-            string nome = nomeEntry.Text;
-            string telefone = telefoneEntry.Text;
-            string email = emailEntry.Text;
-            string endereco = enderecoEntry.Text;
-            string cnpj = cnpjEntry.Text;
-            string tipoFornecimento = tipoFornecimentoEntry.Text;
-
-            // Exemplo: Mostrar uma mensagem com os dados inseridos
-            DisplayAlert("Cadastro", $"Nome: {nome}\nTelefone: {telefone}\nEmail: {email}\nEndereço: {endereco}\nCNPJ: {cnpj}\nTipo de Fornecimento: {tipoFornecimento}", "OK");
-        }
     }
 }
